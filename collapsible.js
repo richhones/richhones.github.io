c711ee927f460f2851cb75e3d380ba40c004c5b6
Rich Jones <rhj026@gmail.com>
2020-10-24 16:31:24 +0100

    before accordian

--- collapsible.js
+++ collapsible.js
@@ -1,12 +1,14 @@
 function toggleDarkMode() {
     var element = document.body;
     if (element.classList.toggle("dark-mode")) {
-        document.getElementById("dark-mode-button").innerHTML="Its too dark";
+        document.getElementById("dark-mode-button").innerHTML="It's too dark";
         localStorage.isDark = true;
+        document.getElementById("linkedin").src="linkedin2.svg"
     }
     else {
-        document.getElementById("dark-mode-button").innerHTML="Its too bright";
+        document.getElementById("dark-mode-button").innerHTML="It's too bright";
         localStorage.isDark = false;
+        document.getElementById("linkedin").src="linkedin.svg"
     } 
 }
 function setDarkModeToPrevious() {
